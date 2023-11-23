63418163d106c127b48f871c0074f6c1d2c7c02d
hoanh <hoanh4113@gmail.com>
2023-11-23 16:46:37 -0500

    handling request.method == 'POST' and getting data

--- website/auth.py
+++ website/auth.py
@@ -1,13 +1,20 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template ("login.html", text="Balls")
 
-@auth.route('/signup')
+@auth.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        name = request.form.get('name')
+        password = request.form.get('password')
+        
     return render_template ("signup.html")
 
 @auth.route('/logout')
